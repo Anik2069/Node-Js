4f052ea21634a7b02cca02b18b0e843ba237cd22
Md Anik Islam <anikislam668@gmail.com>
2020-09-28 13:35:23 +0600

    Module Pattern

--- Hello/require_and_module.js
+++ Hello/require_and_module.js
@@ -2,4 +2,22 @@
 var counter  = function (arr) {
 	console.log("There are "+arr.length+ " elements in this array");
 };
-module.exports = counter;
+
+var adder =  function(a,b){
+	return `The sumation of two number ${a+b}`;
+};
+var pie=3.156
+/*//Way 01
+module.exports = counter;*/
+/*//Way 02
+module.exports.adder = adder */
+//way 03
+/*module.exports.adder =  function(a,b){
+	return `The sumation of two number ${a+b}`;
+};*/
+//way 04
+module.exports = {
+	adder:adder,
+	counter:counter,
+	pie:pie,
+};
