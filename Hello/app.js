559239c0167b03d5acfd04602d40f9814039f48f
Md Anik Islam <anikislam668@gmail.com>
2020-09-28 13:12:05 +0600

    Add Require moduel and use in A file

---
+++ Hello/app.js
@@ -0,0 +1,2 @@
+var counter= require('./require_and_module');
+counter(["1","2","3","4"]);
---
+++ Hello/require_and_module.js
@@ -0,0 +1,5 @@
+//Require Module Node js
+var counter  = function (arr) {
+	console.log("There are "+arr.length+ " elements in this array");
+};
+module.exports = counter;
